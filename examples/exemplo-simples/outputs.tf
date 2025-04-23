90824c28d565bf8737779a99f26bd067bf960fd8
ArlindoSilva <arlindoas2@gmail.com>
2025-04-23 10:44:03 -0300

    Adicionando exemplos

---
+++ examples/exemplo-simples/outputs.tf
@@ -0,0 +1,25 @@
+output "stack_wp_lb_ip" {
+  value       = module.wp_stack.wp_lb_ip
+  description = "IP do Load Balancer"
+}
+
+output "stack_wp_vm_ips" {
+  value       = module.wp_stack.wp_vm_ips
+  description = "IPs das máquinas WordPress"
+}
+
+output "stack_nfs_vm_ips" {
+  value = module.wp_stack.nfs_vm_ip
+  description = "IP da máquina NFS"
+}
+
+output "stack_wp_db_user" {
+  value       = module.wp_stack.wp_db_user
+  description = "Usuário do banco de dados"
+}
+
+output "stack_wp_db_pwd" {
+  value       = module.wp_stack.wp_db_pwd
+  description = "Senha do banco de dados"
+  sensitive   = true // Com esse atributo a senha não é mostrada no output
+}
